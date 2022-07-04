2eda1f4e93491828fbd722e96ceddc16e4a17a77
JavierWangRuoFei <bdwrfcug@163.com>
2022-07-04 22:18:45 +0800

    Update FileURLServiceImpl.java

--- file-upload/src/main/java/com/javier/file/service/impl/FileURLServiceImpl.java
+++ file-upload/src/main/java/com/javier/file/service/impl/FileURLServiceImpl.java
@@ -17,10 +17,10 @@ public class FileURLServiceImpl implements FileURLService {
         // Endpoint以杭州为例，其它Region请按实际情况填写。
         endpoint = "http://oss-cn-hangzhou.aliyuncs.com";
         // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
-        accessKeyId = "LTAIk20LehTjbi9c";
-        accessKeySecret = "9f6W7FKK46G28JXcxn61ILp85yjRmc";
-        bucketName = "youdaobiji";
-        objectName = "589595.jpg";
+        accessKeyId = "88";
+        accessKeySecret = "99";
+        bucketName = "00";
+        objectName = "88.jpg";
 
         // 创建OSSClient实例。
         OSSClient ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret);
